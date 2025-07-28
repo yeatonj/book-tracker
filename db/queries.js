d77f3cd161927ee400f55838d1d40aa3949456ff
Josh Yeaton <yeaton.josh@gmail.com>
2025-07-28 19:16:29 -0400

    Expand book query to include tags and genres

--- db/queries.js
+++ db/queries.js
@@ -1,9 +1,14 @@
 const pool = require("./pool");
 
 async function getAllBooks() {
-  const { rows } = await pool.query("SELECT books.title, authors.name AS author, books.pages, books.summary FROM books INNER JOIN authors ON books.author_id=authors.id;");
+  const { rows } = await pool.query("SELECT books.title, authors.name AS author, books.pages, books.summary, string_agg(tags.tag, ', ') AS tags, string_agg(genres.genre, ', ') AS genres FROM books INNER JOIN booktags ON books.id=booktags.book_id INNER JOIN tags ON booktags.tag_id=tags.id INNER JOIN authors ON books.author_id=authors.id INNER JOIN bookgenres ON books.id=bookgenres.book_id INNER JOIN genres ON bookgenres.genre_id=genres.id GROUP BY authors.name, books.pages, books.summary, books.title;");
   return rows;
 }
+
+// async function getAllAuthors() {
+//   const { rows } = await pool.query("SELECT books.title, authors.name AS author, books.pages, books.summary FROM books INNER JOIN authors ON books.author_id=authors.id;");
+//   return rows;
+// }
 
 module.exports = {
   getAllBooks,
